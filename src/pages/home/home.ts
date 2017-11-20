0ea8039a29b1a8b14eaa05951a5c49847268f680
Samantha Jayasinghe <isamantha@gmail.com>
2017-11-21 09:33:32 +1300

    add sql-lite storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,6 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
   }
 
 }
